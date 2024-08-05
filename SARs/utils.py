e9bba34a907e73d3e2166340040da42acc955762
zhou_qiang98 <zhou_qiang98@163.com>
2024-08-05 09:39:58 +0800

    修改utils，自动生成训练名字

--- SARs/utils.py
+++ SARs/utils.py
@@ -19,8 +19,43 @@ def config_to_dict(file_path):
 
     return config_dict
 
+from datetime import datetime
+
+def get_formatted_time():
+    """
+    获取当前时间并格式化为 'YYYY-MM-DD' 格式
+
+    返回:
+    str: 格式化后的时间字符串
+    """
+    # 获取当前时间
+    current_time = datetime.now()
+
+    # 格式化时间
+    formatted_time = current_time.strftime('%Y-%m-%d')
+
+    return formatted_time
+
+def generate_name(naive_name=None):
+    """
+    生成格式化时间和6位序列号组成的名字
+
+    返回:
+    str: 生成的名字
+    """
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y-%m-%d')
+    sequence_number = current_time.strftime('%H%M%S%f')[:6]  # 使用当前时间的秒和微秒生成6位序列号
+    name = f"{formatted_time}-{sequence_number}"
+    if naive_name is None:
+        return name
+    else:
+        return naive_name + '-' + name
+
 if __name__ == '__main__':
-    from objprint import op
-    file_path = 'SARs/default_training_setting.py'
-    config_dict = config_to_dict(file_path)
-    op(config_dict)
+    # from objprint import op
+    # file_path = 'SARs/default_training_setting.py'
+    # config_dict = config_to_dict(file_path)
+    # op(config_dict)
+
+    print(generate_name())
